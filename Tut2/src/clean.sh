1072e0b86ac9640789ce0b47f082edf3e899e0eb
Murray Heymann <heymann.murray@gmail.com>
2016-08-14 15:39:50 +0200

    currently, sender deconstructs and sends a file in an unreliable, but otherwise hopefully functioning way

---
+++ Tut2/src/clean.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+rm -rv ./*.class
+rm -rv filereceiver/*.class
+rm -rv filesender/*.class
+rm -rv layouts/*.class
--- Tut2/src/filesender/SenderDeconstructor.java
+++ Tut2/src/filesender/SenderDeconstructor.java
@@ -4,20 +4,24 @@ import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.net.*;
+import parameters.*;
 
 public class SenderDeconstructor implements Runnable {
 	private String fileLocation = null;
+	private long fileSize = -1;
 	private String IP_Address = null;
 	private Sender sender = null;
 	private int port = -1;
 
 	private SocketChannel socketChannel = null;
 	private DatagramChannel datagramChannel = null;
+	private InetSocketAddress address = null;
 	private Selector selector = null;
 
-	public SenderDeconstructor(String fileLocation, String IP_Address, int port, Sender sender) 
+	public SenderDeconstructor(String fileLocation, long fileSize, String IP_Address, int port, Sender sender) 
 	{
 		this.fileLocation = fileLocation;
+		this.fileSize = fileSize;
 		this.IP_Address = IP_Address;
 		this.sender = sender;
 		this.port = port;
@@ -31,17 +35,58 @@ public class SenderDeconstructor implements Runnable {
 	}
 
 	public void go() {
+		ByteBuffer sendBuff = ByteBuffer.allocate(Parameters.BUFFER_SIZE);
+		ByteBuffer readBuff = ByteBuffer.allocate(Parameters.DATA_BYTES);
+		FileInputStream fin = null;
+		FileChannel fcin = fin.getChannel();
+
+		try {
+			fin =new FileInputStream(this.fileLocation);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		int r = 0;
+		int sequenceNo = 0;
+
+		for (sequenceNo = 0; true; sequenceNo++) {
+			sendBuff.clear();
+			readBuff.clear();
+
+			try {
+				r = fcin.read(readBuff);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			sendBuff.putInt(sequenceNo);
+			sendBuff.putInt(r);
+			sendBuff.put(readBuff);
+
+			if(r == -1) {
+				break;
+			}
+			sendBuff.flip();
+			/* probably unnecessary */
+			readBuff.flip();
+
+			try {
+				this.datagramChannel.send(sendBuff, this.address);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 
 	}
 
 	public boolean connect() {
+		this.address = new InetSocketAddress(this.IP_Address, this.port);
 		try {
 			this.selector = Selector.open();
 			this.datagramChannel = DatagramChannel.open();
 			this.datagramChannel.configureBlocking(false);
 			this.socketChannel = SocketChannel.open();
 			this.socketChannel.configureBlocking(false);
-			this.socketChannel.connect(new InetSocketAddress(this.IP_Address, this.port));
+			this.socketChannel.connect(this.address);
 			while (!this.socketChannel.finishConnect());
 		} catch (IOException e) {
 			sender.appendTCP("IOException: failed to create SocketChannel\n");
@@ -50,16 +95,12 @@ public class SenderDeconstructor implements Runnable {
 		sender.appendTCP("Set up TCP connection\n");
 
 		DatagramSocket dSocket = datagramChannel.socket();
+		/* TODO: since we arent receiving back, is it necessary to bind? */
 		try {
-			dSocket.bind(new InetSocketAddress(this.IP_Address, this.port));
+			dSocket.bind(this.address);
 		} catch (SocketException e) {
 			System.out.printf("Socket Exception\n");
 			e.printStackTrace();
-			/*
-		} catch (IOException e) {
-			System.out.printf("IOException\n");
-			e.printStackTrace();
-			*/
 		}
 
 		try {
